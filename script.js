ba6a9ee991568d1ee5d7082966d422b03b7043ff
Nelson AD <ndemoraes93@gmail.com>
2019-04-08 22:30:24 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+
+let STATE_On_Bio=true;
+
+function selectInactive(){
+    // alert("in function");
+    //on click
+    $('.projects-button' ).click(function() {
+        // alert( "Handler for .click() called." );
+
+        //update bio button to not greyed
+        // let currentButton = $('.current');
+        // let inactiveButton = $('.inactive');
+        // inactiveButton.toggleClass('inactive current');
+        // currentButton.toggleClass('current inactive');
+        // inactiveButton.parent().closest("button").toggleClass('current inactive');
+
+        let bioSection = $('#mainSection');
+        bioSection.toggleClass('hidden');
+        let projectSection = $('#projectSection');
+        projectSection.toggleClass('hidden');
+        console.log(STATE_On_Bio);
+        STATE_On_Bio=!STATE_On_Bio;
+        console.log(STATE_On_Bio);
+
+        if (STATE_On_Bio === true){
+            let sectionButton = $('.projects-button');
+            sectionButton.text('Projects');
+        }
+
+        else {
+            let sectionButton = $('.projects-button');
+            sectionButton.text('Bio');
+        }
+
+
+
+      });
+
+    //update bio button to not greyed
+
+    // selectInactive();
+}
+
+$(selectInactive);
